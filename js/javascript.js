dea5834a6ce4a2b9fd1223e9a7b668b574e9aae7
mahdie <m119.ahmadi@gmail.com>
2020-09-11 21:35:21 +0430

    add-animation

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,23 @@
-var text = 'PLEASE MEAT OUR TEAM';
+var text = 'PLEASE MEAT OUR TEAM |';
 var i=0;
 var typing = () => {
-if( i < text.length) {
+if(i === text.length -1 ){
+    setInterval(() => {
+         if(i === text.length -1){
+          document.querySelector('.heading').innerHTML = 'PLEASE MEAT OUR TEAM |';
+          i++
+         }else if(i === text.length){
+          document.querySelector('.heading').innerHTML = 'PLEASE MEAT OUR TEAM ';
+          i--
+         }
+    }, 300);
+
+}else if( i < text.length) {
      document.querySelector('.heading').innerHTML += text.charAt(i);
      i++;
      setTimeout(typing,150);
-     }
+}   
+
 }
+
 typing();
